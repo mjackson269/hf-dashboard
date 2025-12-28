3a30cece2a6ab72b0ba8006d39c53a7a7a9d458d
Matthew Jackson <matthewjackson81@hotmail.com>
2025-12-28 22:56:06 +0000

    Fix Tailwind content paths for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
