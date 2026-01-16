bca2a9aeaa242cee5805ddc10512a21eb71f04ab
Matthew Jackson <matthewjackson81@hotmail.com>
2026-01-16 19:30:11 +0000

    Add props interface to BandTable to fix TS error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import "./globals.css";
 
 export const metadata = {
@@ -10,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <ClientWrapper>{children}</ClientWrapper>
+      </body>
     </html>
   );
 }
